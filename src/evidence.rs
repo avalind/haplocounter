4b6d40366c226a4491fc317e43aec6e440f2e06c
Anders Valind <anders.valind@med.lu.se>
2022-02-08 22:26:07 +0000

    More work

---
+++ src/evidence.rs
@@ -0,0 +1,121 @@
+use std::collections::HashMap;
+
+#[derive(Debug, Clone, Copy)]
+pub enum EvidenceType {
+    Maternal,
+    Transmitted,
+    Reference,
+}
+
+pub struct EvidenceTable {
+    data: HashMap<String, HashMap<(u32, i64), Vec<(EvidenceType, i32)>>>,
+}
+
+#[derive(Debug)]
+pub struct BarcodeTally {
+    pub barcode: String,
+    pub transmitted_count: i32,
+    pub maternal_count: i32,
+    pub reference_count: i32,
+}
+
+impl BarcodeTally {
+    pub fn new(barcode: &str) -> BarcodeTally {
+        BarcodeTally{
+            barcode: barcode.to_string(),
+            transmitted_count: 0,
+            maternal_count: 0,
+            reference_count: 0,
+        }
+    }
+
+    #[inline(always)]
+    pub fn update(&mut self, offset: (i32, i32, i32)) {
+        self.maternal_count += offset.0;
+        self.transmitted_count += offset.1;
+        self.reference_count += offset.2;
+    }
+
+    pub fn haplotype_class(self) -> EvidenceType {
+        if self.maternal_count <= self.transmitted_count {
+            EvidenceType::Transmitted
+        } else {
+            EvidenceType::Maternal
+        }
+    }
+}
+
+impl EvidenceTable {
+    pub fn new() -> EvidenceTable {
+        let t = EvidenceTable{ data: HashMap::new() };
+        t
+    }
+
+    pub fn update_site_data(&mut self, barcode: &str, location: (u32, i64), count: i32, evt: EvidenceType) {
+        if !self.data.contains_key(barcode) {
+            self.data.insert(barcode.to_string(), HashMap::new());
+        };
+
+        // subtab is now a mutable reference into the hashmap, so we update through it.
+        let subtab = match self.data.get_mut(barcode) {
+            Some(v) => v,
+            None => { panic!("barcode not found in table!\n"); },
+        };
+
+        if !subtab.contains_key(&location) {
+            let mut v: Vec<(EvidenceType, i32)> = Vec::new();
+            v.push((evt, count));
+            subtab.insert(location, v); 
+        } else {
+            let vecref = match subtab.get_mut(&location) {
+                Some(v) => v,
+                None => { panic!("location not found in subtable\n"); },
+            };
+            vecref.push((evt, count));
+        }       
+    }
+
+    pub fn prettyprint(self) {
+        for (k, v) in self.data.iter() {
+            println!("{} = {:?}", k, v);
+        }
+    }
+
+    pub fn tally_barcode(data: &HashMap<(u32, i64), Vec<(EvidenceType, i32)>>, barcode: &str) -> BarcodeTally {
+        let mut tally = BarcodeTally::new(barcode);
+        for (k, v) in data.iter() {
+            let adds = {
+                let mut mats: i32 = 0;
+                let mut trans: i32 = 0;
+                let mut refs: i32 = 0;
+                v.iter().for_each(|v| {
+                    match *v {
+                        (EvidenceType::Maternal, count) => mats += count,
+                        (EvidenceType::Transmitted, count) => trans += count,
+                        (EvidenceType::Reference, count) => refs += count,
+                    }
+                });
+                (mats, trans, refs)
+            };
+            tally.update(adds);
+        }
+        tally  
+    }
+
+    pub fn classify(self) {
+        let mut tallies: Vec<BarcodeTally> = Vec::new();
+        for (k, v) in self.data.iter() {
+            let barcode_summary = EvidenceTable::tally_barcode(&v, k);
+            println!("{}\t{:?}", k, barcode_summary.haplotype_class());
+        }
+    }
+}   
+
+/*fn main() {
+    let mut tab = EvidenceTable::new();
+    tab.update_site_data(&"ACGTGA", (20, 12334534), 1, EvidenceType::Maternal);   
+    tab.update_site_data(&"ACGTGA", (20, 12334534), 1, EvidenceType::Reference);
+    tab.update_site_data(&"ACGTTT", (20, 12334112), 1, EvidenceType::Transmitted);
+    tab.update_site_data(&"ACGTGA", (20, 12312321), 1, EvidenceType::Maternal);
+    tab.prettyprint()
+}*/
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use clap::Parser;
 use rust_htslib::{bam, bam::Read, bam::record::Aux};
 use rust_htslib::bcf::{Reader, Read as BcfRead, record as BcfRecord};
 
+mod evidence;
+
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
@@ -16,25 +18,60 @@ struct Args {
     /// the sample in the vcf file that corresponds
     /// to the mother
     #[clap(long)]
-    name_mother: String,
+    name_mother: Option<String>,
 
     /// the sample in the vcf file that corresponds to
     /// the proband
     #[clap(long)]
-    name_proband: String,
+    name_proband: Option<String>,
+
+    /// whether we should run in paternal mode
+    #[clap(long)]
+    paternal: bool,
+
+    /// if we run in paternal mode, we need the sample name of the 
+    /// father in the vcf file
+    #[clap(long)]
+    name_father: Option<String>,
 }
 
-enum EvidenceType {
-    Maternal,
-    Transmitted,
-}
+fn tally_evidence(evt: &mut evidence::EvidenceTable, var: &BcfRecord::Record, pileups: &bam::pileup::Pileup, vartype: evidence::EvidenceType)
+{
+    for aln in pileups.alignments() {
+        // Unpack the cellular barcode this read/alignment came from.
+        let alnrec = aln.record();
+        let barcode = match alnrec.aux(b"CB") {
+            Ok(v) => {
+                if let Aux::String(s) = v {
+                    s
+                } else {
+                    panic!("CB tag found but wrong type!");
+                }
+            },
+            _ => continue,
+        };
 
-// a struct that represents evidence at
-// a single position for either
-struct PositionalEvidence {
-    cid: i32,
-    pos: i64,
-    evtype: EvidenceType,
+        let readpos = match aln.qpos() {
+            Some(v) => v,
+            None => continue, // None here means that the read is deleted/ref missing and if so we skip to the next alignment.
+        };
+        let nt = aln.record().seq()[readpos] as char;
+        // find the first alt allele and check that its only a SNV.
+        let alt = {
+            let alt_first = var.alleles()[1];
+            if alt_first.len() > 1 {
+                continue;
+            } else {
+                alt_first[0] as char
+            }
+        };
+
+        if alt == nt {
+            evt.update_site_data(&barcode, (var.rid().unwrap(), var.pos()), 1, vartype);
+        } else {
+            evt.update_site_data(&barcode, (var.rid().unwrap(), var.pos()), 1, evidence::EvidenceType::Reference);
+        }
+    }
 }
 
 
@@ -42,70 +79,50 @@ fn main() {
     let args = Args::parse();   
     let mut vcf = Reader::from_path(&args.vcf_path).expect("Unable to open vcf");
     let mut aln = bam::IndexedReader::from_path(&args.bam_path).expect("Unable to open bam");
-    let header_view = vcf.header().clone();     
+    let header_view = vcf.header().clone();
+    let mut tab = evidence::EvidenceTable::new();    
 
-    let mother_id = header_view.sample_id(&args.name_mother.as_bytes()).unwrap();
-    println!("{}", mother_id);
+    if args.paternal {
+        let father_sample_id = match(args.name_father) {
+            Some(name) => name,
+            None => panic!("--name_father needs to be supplied when running in paternal mode"),
+        };
+        let father_id = header_view.sample_id(&father_sample_id.as_bytes()).unwrap();
 
-    for (idx, rr) in vcf.records().enumerate() {
-        let mut rec = rr.expect("Unable to read record!");
-        let ctg = match rec.rid() {
-            Some(r) => r,
-            None => panic!("malformed vcf file, panicking!"),
+    } else {
+        let name_mother = match(args.name_mother) {
+            Some(name) => name,
+            None => panic!("--name_mother needs to be supplied when running in non-paternal mode"),
         };
 
-        aln.fetch((header_view.rid2name(ctg).unwrap(), rec.pos(), rec.pos()+1));
-        for p in aln.pileup() {
-            let pileup = p.unwrap();
-            if i64::from(pileup.pos()) == rec.pos() {
+        let mother_id = header_view.sample_id(&name_mother.as_bytes()).unwrap();
+
+        for (idx, rr) in vcf.records().enumerate() {
+            let mut rec = rr.expect("Unable to read record!");
+            let ctg = match rec.rid() {
+                Some(r) => r,
+                None => panic!("malformed vcf file, panicking!"),
+            };
+
+            aln.fetch((header_view.rid2name(ctg).unwrap(), rec.pos(), rec.pos()+1));
+            for p in aln.pileup() {
+                let pileup = p.unwrap();
+                if i64::from(pileup.pos()) == rec.pos() {
                 // we are now in a position with a variant. next step is to check whether
                 // this variant comes from the transmitted X or the one the mother did not
                 // transmit. we do this by looking at the genotypes for the maternal sample
                 // if they all are 0, we know that the variant is on the transmitted chromosome
                 // (because of how the input vcf file is generated)
-                let gt = rec.genotypes().expect("Unable to read genotypes!");               
-                if gt.get(mother_id).iter().all(|x| *x == BcfRecord::GenotypeAllele::Unphased(0)) {
-                    // we know now we are on a variant present _ONLY_ on the transmitted X,
-                    // and we thus proceed to count the number nucleotides at this position.
-                    for alignment in pileup.alignments() {
-                        let alnrec = alignment.record();
-                        let barcode = match alnrec.aux(b"CB") {
-                            Ok(val) => { 
-                                if let Aux::String(s) = val {
-                                    s
-                                } else {
-                                    // this really shouldn't happen.
-                                    panic!("Found CB tag but it isn't a string, bailing!");
-                                }
-                            },
-                            _ => "",
-                        };
-                        
-                        let readpos = match alignment.qpos() {
-                            Some(v) => v,
-                            None => continue,
-                        };
-    
-                        if readpos >= alnrec.seq_len() {
-                            panic!("readpos >= read length, this should REALLY NOT happen!");
-                        };
-
-                        let seqdata = alnrec.seq();
-                        let nt = seqdata[readpos] as char;
-                        let alt = rec.alleles()[1];
-                        if alt.len() > 1 {
-                            continue;
-                        } else {
-                            let altnuc = alt[0] as char;
-                            println!("[{:?}] - [{:?}]", nt, altnuc);
-                        }
+                    let gt = rec.genotypes().expect("Unable to read genotypes!");               
+                    if gt.get(mother_id).iter().all(|x| *x == BcfRecord::GenotypeAllele::Unphased(0)) {
+                        tally_evidence(&mut tab, &rec, &pileup, evidence::EvidenceType::Transmitted);
+                    } else {
+                        tally_evidence(&mut tab, &rec, &pileup, evidence::EvidenceType::Maternal);
                     }
-                } else {
-                    println!("Variant counted as coming from the kept maternal homologue!");
-                }
                 
-                println!("{}:{} depth {}", pileup.tid(), pileup.pos(), pileup.depth());
+                }
             }
         }
+        tab.classify();
     }
 }
